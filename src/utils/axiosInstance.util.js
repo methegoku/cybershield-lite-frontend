2d44b52a0152f0bdde4b8d9c226be0cb8788c5af
Gokul Elumalai <gokulelumalai46@gmail.com>
2025-07-01 13:52:57 +0530

    Update axiosInstance.util.js

--- src/utils/axiosInstance.util.js
+++ src/utils/axiosInstance.util.js
@@ -15,18 +15,22 @@ axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
-
   (error) => {
     if (error.response) {
       if (error.response.status === 400) {
-        toast.error(error);
-        window.location.href("/");
+        toast.error(error.response.data.message || "Bad Request");
+        window.location.href = "/";
       } else if (error.response.status === 500) {
         toast.error("Server Error. Please try again.");
+      } else {
+        toast.error(error.response.data.message || "An error occurred.");
       }
     } else if (error.code === "ECONNABORTED") {
-      toast.error("Request timeout.");
+      toast.error("Request timeout. Please try again.");
+    } else {
+      toast.error("Network Error. Please check your connection.");
     }
+    return Promise.reject(error); // ensure error propagation
   }
 );
 
